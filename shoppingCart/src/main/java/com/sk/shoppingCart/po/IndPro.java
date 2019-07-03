714f0cbc770cb0d05cda1d20b78f70c12b7039b1
Administrator <394487194@qq.com>
2019-07-03 08:53:14 +0800

    订单页面修改，接口初步定义

---
+++ shoppingCart/src/main/java/com/sk/shoppingCart/po/IndPro.java
@@ -0,0 +1,77 @@
+package com.sk.shoppingCart.po;
+
+
+public class IndPro {
+
+  private long ipId;
+  private String indentId;
+  private String productId;
+  private long indentNum;
+  private double productPrice;
+  private String productName;
+  private String productPic;
+
+
+  public long getIpId() {
+    return ipId;
+  }
+
+  public void setIpId(long ipId) {
+    this.ipId = ipId;
+  }
+
+
+  public String getIndentId() {
+    return indentId;
+  }
+
+  public void setIndentId(String indentId) {
+    this.indentId = indentId;
+  }
+
+
+  public String getProductId() {
+    return productId;
+  }
+
+  public void setProductId(String productId) {
+    this.productId = productId;
+  }
+
+
+  public long getIndentNum() {
+    return indentNum;
+  }
+
+  public void setIndentNum(long indentNum) {
+    this.indentNum = indentNum;
+  }
+
+
+  public double getProductPrice() {
+    return productPrice;
+  }
+
+  public void setProductPrice(double productPrice) {
+    this.productPrice = productPrice;
+  }
+
+
+  public String getProductName() {
+    return productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+
+
+  public String getProductPic() {
+    return productPic;
+  }
+
+  public void setProductPic(String productPic) {
+    this.productPic = productPic;
+  }
+
+}
